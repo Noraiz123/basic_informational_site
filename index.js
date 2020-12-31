615c6700ce18f53a6ee54bf1f1a3012ab8f70f4f
leavism <ghuydang@gmail.com>
2020-12-31 15:46:59 -0800

    Converted to an Express app

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-const http = require('http')
-const url = require('url')
-const fs = require('fs')
+const express = require('express')
+const path = require('path')
 
-const server = http.createServer((req, res) => {
-  const query = url.parse(req.url, true)
-  let filename = (query.pathname === '/' ? './index.html' : '.' + query.pathname)
+const app = express()
+const port = 8080
 
-  fs.readFile(filename, (err, data) => {
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/html' })
-      return res.end('404 Not Found')
-    }
-    if (filename === '') {
-      filename = 'index.html'
-      res.writeHead(200, { 'Content-Type': 'text/html' })
-      res.write(data)
-      return res.end()
-    }
-
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.write(data)
-    return res.end()
-  })
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-server.listen(8080, 'localhost', () => {
-  console.log('Server is running.')
+app.get('/about.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '/about.html'))
+})
+
+app.get('/contact-me.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '/contact-me.html'))
+})
+
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '404.html'))
+})
+app.listen(port, () => {
+  console.log('Running app...')
 })
